c77aa5ddc9b0cb3c6eddf6cdfeb07baad2597000
Linh Van Nguyen <linh.nguyen04@hastech.vn>
2022-07-11 16:46:38 +0700

    first commit

---
+++ src/main/java/com/example/hello_world/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping
+  public String hello() {
+    return "Hello world!";
+  }
+}
